8e2a37a3e78c1630c465943efa1a857f902514a7
Krzysztof Krakowiak <komumor@wp.pl>
2022-09-12 13:18:44 +0200

    Updates

---
+++ precache-manifest.10fd8402e50d475c1cd3099e5d29b4d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a93af348b6ff23f9b47a",
+    "url": "/TodoApp/static/css/main.92f0e4fe.chunk.css"
+  },
+  {
+    "revision": "a93af348b6ff23f9b47a",
+    "url": "/TodoApp/static/js/main.276316d2.chunk.js"
+  },
+  {
+    "revision": "b429d889341aa25a2548",
+    "url": "/TodoApp/static/js/runtime~main.5a342d52.js"
+  },
+  {
+    "revision": "41de3979ba14dc24608d",
+    "url": "/TodoApp/static/js/2.5fb77470.chunk.js"
+  },
+  {
+    "revision": "f4fed377a9c3a19d5528b9d1d4f94687",
+    "url": "/TodoApp/index.html"
+  }
+];
